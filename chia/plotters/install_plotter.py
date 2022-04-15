f611ea033d977242cb24ecfdeea9f67ad77d563f
ChiaMineJP <admin@chiamine.jp>
2022-04-16 00:16:19 +0900

    Cleaned up `install_plotter.py`

--- chia/plotters/install_plotter.py
+++ chia/plotters/install_plotter.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 from chia.plotters.bladebit import install_bladebit
 from chia.plotters.madmax import install_madmax
@@ -9,25 +8,17 @@ def install_plotter(plotter: str, root_path: Path):
         print("Chiapos already installed. No action taken.")
         return
     elif plotter == "madmax":
-        if not os.path.exists(root_path / "madmax-plotter/build/chia_plot"):
-            print("Installing madmax plotter.")
-            try:
-                install_madmax(root_path)
-            except Exception as e:
-                print(f"Exception while installing madmax plotter: {e}")
-            return
-        else:
-            print("Madmax plotter already installed.")
+        try:
+            install_madmax(root_path)
+        except Exception as e:
+            print(f"Exception while installing madmax plotter: {e}")
+        return
     elif plotter == "bladebit":
-        if not os.path.exists(root_path / "bladebit/.bin/release/bladebit"):
-            print("Installing bladebit plotter.")
-            try:
-                install_bladebit(root_path)
-            except Exception as e:
-                print(f"Exception while installing bladebit plotter: {e}")
-                return
-        else:
-            print("Bladebit plotter already installed.")
+        try:
+            install_bladebit(root_path)
+        except Exception as e:
+            print(f"Exception while installing bladebit plotter: {e}")
+        return
     else:
         print("Unknown plotter. No action taken.")
         return
